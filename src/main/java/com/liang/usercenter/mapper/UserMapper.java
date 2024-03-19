a95e5f12bd09cfc0e90cb93b57b502c4de8e0816
xiao-xiao-liang <2901815578@qq.com>
2024-03-19 20:53:09 +0800

    用户中心后端初始化及测试

---
+++ src/main/java/com/liang/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.liang.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liang.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
